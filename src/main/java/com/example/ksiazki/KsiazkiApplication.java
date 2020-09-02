952738e9cedcb7560ddda45a1f92c2495afc6d2e
Kamil Rogala <rogalak25@gmail.com>
2020-09-02 10:59:21 +0200

    "First version"

---
+++ src/main/java/com/example/ksiazki/KsiazkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ksiazki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsiazkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsiazkiApplication.class, args);
+	}
+
+}
